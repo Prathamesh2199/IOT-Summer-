b270d7807f8c1d42f2c24d5aba69734a4a47219b
Prathamesh Patil <48598415+Prathamesh2199@users.noreply.github.com>
2019-06-18 15:47:19 +0530

    Add files via upload
    smoke sensor

---
+++ mq2.ino
@@ -0,0 +1,31 @@
+
+int led = 10;
+int smokeA0 = A5;
+
+// Your threshold value. You might need to change it.
+int sensorThres = 400;
+
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(smokeA0, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int analogSensor = analogRead(smokeA0);
+
+  Serial.print("Pin A0: ");
+  Serial.println(analogSensor);
+  // Checks if it has reached the threshold value
+  if (analogSensor > sensorThres)
+  { digitalWrite(led,HIGH);
+
+}
+
+else {
+
+digitalWrite(led,LOW);
+
+}
+  delay(100);
+}
